e874d124b393aa9410a649a48a6dd4551223a519
1012kratikavarshney <it23146@glbitm.ac.in>
2026-06-26 11:26:23 +0530

    merge remote repo

---
+++ notification-app-fe/src/assets/notificattions.jsx
@@ -0,0 +1,33 @@
+const notifications = [
+
+{
+id:1,
+type:"Placement",
+message:"Google Hiring Drive",
+time:"10 min ago"
+},
+
+{
+id:2,
+type:"Result",
+message:"Mid Semester Result Declared",
+time:"30 min ago"
+},
+
+{
+id:3,
+type:"Event",
+message:"Tech Fest Tomorrow",
+time:"1 hour ago"
+},
+
+{
+id:4,
+type:"Placement",
+message:"Microsoft Internship",
+time:"2 hours ago"
+}
+
+];
+
+export default notifications;
